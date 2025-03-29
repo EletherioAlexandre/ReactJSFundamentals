17c6634d2ec3624999e508233ed9d22195b5668a
EletherioAlexandre <eletherioalexandre@gmail.com>
2025-03-29 18:52:40 -0300

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Post } from "./Post";
+import "./global.css"
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Jax Teller"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis excepturi nihil neque delectus similique amet adipisci eligendi ad autem deleniti dolorum odit quasi, magnam atque expedita eum, minima nisi rem!"
